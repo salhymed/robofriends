0ed6f01d47a65198d3355a6eaa86b1d65432f72e
unknown <samed.salhy@hotmail.com>
2022-08-16 21:59:52 +0100

    adding Error Boundry fixed

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -2,9 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({listrobots}) => {
-    if(true){
-        throw new Error('Oh No !');
-    }
     const cards = listrobots.map((r, i) => {
         return <Card key={i} name={r.name} email={r.email} />;
         });
