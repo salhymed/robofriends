5254621a8d632b39354ad06c6fadad72f6a32434
unknown <samed.salhy@hotmail.com>
2022-09-16 20:57:24 +0100

    fix test

---
+++ src/Components/Card.test.js
@@ -0,0 +1,12 @@
+import React  from "react";
+import { shallow } from 'enzyme';
+import Card from './Card';
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-preact-pure';
+
+configure({ adapter: new Adapter });
+
+it('should render 3', () => {
+    const p = 4;
+    expect(p).toEqual(4);
+})
